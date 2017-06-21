a58864b71a409d99f955283e375d8e3a94d59a1e
Kevin Chavez <aykev@fb.com>
2017-06-21 01:09:52 -0700

    upgrade to webpack 3

--- bin/includes.js
+++ bin/includes.js
@@ -4,9 +4,11 @@ const path = require("path");
 const theme_pkg = require(path.join(process.cwd(), 'package.json'))
 const file = path.join(process.cwd(), theme_pkg.main);
 
-module.exports = `
+module.exports = () => ({
+  code: `
 console.log('~ spur web ~');
 console.log('${file}')
 module.exports = require('${file}')
 `
+})
 
